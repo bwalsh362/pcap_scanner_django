93d9dbd28c1f428e3627a16c38fa511b3bfed02b
bwalsh <bwalsh362@gmail.com>
2018-01-13 17:29:10 -0700

    Working on adding images to screen

--- pcap_scanner_app/static/js/topology-canvas.js
+++ pcap_scanner_app/static/js/topology-canvas.js
@@ -1,27 +1,24 @@
-
-var divwidth = document.getElementById('main-div').offsetWidth-40;
-canvas.width = divwidth;
-canvas.height = window.innerHeight;
-
-
-function add_icons(device_list){
-    var canvas = document.getElementById('topology-canvas'),
+var canvas = document.getElementById('canvas'),
     context = canvas.getContext('2d');
-    // for (i=0; i<device_list.length; i++){
+
+var grd = context.createLinearGradient(0,0,200,0);
+grd.addColorStop(0, "red");
+grd.addColorStop(1, "white");
+
+context.fillStyle = grd;
+context.fillRect(10,10,150,80);
+    // context.fillStyle = "rgb(0, 0, 0)";
+    // context.fillRect(10, 30, 30, 30);
+
+add_icons();
+
+function add_icons(){
+    // context.fillRect(20,20,20,20);
+    for(i=0;i<=2;i++){
         base_image = new Image();
         base_image.src = '../static/img/switch.png';
         base_image.onload = function(){
-            context.drawImage(base_image, canvas.width/2 - base_image.width/2, canvas.height/2 - base_image.height/2);
+            context.drawImage(base_image, base_image.width, base_image.height);
         }
+    }
 }
-
-// add_icons();
-//
-// function add_icons() {
-//     base_image = new Image();
-//     base_image.src = '../static/img/switch.png';
-//     base_image.onload = function(){
-//         context.drawImage(base_image, canvas.width/2 - base_image.width/2, canvas.height/2 - base_image.height/2);
-//     };
-// }
-
