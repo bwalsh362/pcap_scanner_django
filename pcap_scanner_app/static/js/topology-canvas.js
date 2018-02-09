19e21b41912451ccc03a348de36fc91d5ca193c5
bwalsh <bwalsh362@gmail.com>
2018-02-09 07:17:40 -0700

    Fixed issues from MongoDB Implementation

--- pcap_scanner_app/static/js/topology-canvas.js
+++ pcap_scanner_app/static/js/topology-canvas.js
@@ -1,47 +1,17 @@
-// var canvas = document.getElementById('canvas');
-// var context = canvas.getContext('2d');
-//     canvas.width = document.body.clientWidth;
-//     canvas.height = document.body.clientHeight;
-
 var imgs = [];
 var imagesOK = 0;
-JS_LIST = formatList(JS_LIST);
-JS_LIST_UNIQ = removeDuplicates(JS_LIST);
 var sources = getSources(JS_LIST);
 loadImages(imagesLoaded);
-
-
-function formatList(devices){
-    devices = devices.replace(/[\[\](]+/g, '');
-    devices = devices.replace(/&#39;/g, '');
-    devices = devices.split('),');
-    return devices;
-}
-
-function removeDuplicates(devices){
-    var unique_array = [];
-    var multi_arr = [];
-    for (var i = 0; i < devices.length; i++){
-        var tmp_array = devices[i].split(',');
-        multi_arr.push(tmp_array);
-    }
-    for (var a = 0; a < multi_arr.length; a++){
-        if(unique_array.indexOf(multi_arr[a][10]) == -1){
-            unique_array.push(multi_arr[a])
-        }
-    }
-    return unique_array;
-}
 
 function getSources (devices){
     var source_arr = [];
     for(var i = 0; i < devices.length; i++){
-        var tmp_array = devices[i].split(',');
-        if (tmp_array[4] === ' 1' && tmp_array[6] === ' 1'){
+        // var tmp_array = devices[i].split(',');
+        if (devices[i][4] === '1' && devices[i][6] === '1'){
             source_arr.push('../static/img/ml_switch.png');
-        }else if (tmp_array[4] === ' 1'){
+        }else if (devices[i][4] === '1'){
             source_arr.push('../static/img/switch.png');
-        }else if (tmp_array[6] === ' 1'){
+        }else if (devices[i][6] === '1'){
             source_arr.push('../static/img/router.png');
         }else{
             console.log('other');
@@ -72,15 +42,12 @@ function imagesLoaded(){
         var button = document.createElement('button');
         button.innerHTML = "<img src=" + sources[i] + "/>";
         button.id = 'button' + i;
-        button.setAttribute('data-info', JS_LIST[i]);
-        var data = button.getAttribute('data-info');
-        var tmp_arr = data.replace(' ', '');
-        tmp_arr = tmp_arr.split(',');
+        $(button).data("info", JS_LIST[i]);
+        var data = jQuery(button).data('info');
         button.setAttribute('onclick', "buttonClicked(this.id)");
-        // button.setAttribute('onmouseover', "buttonHover(this.id)");
         button.setAttribute('data-toggle', "tooltip");
         button.setAttribute('data-placement', "bottom");
-        button.setAttribute('title', tmp_arr[10]);
+        button.setAttribute('title', data[10]);
         var body = document.getElementById("main-div");
         body.appendChild(button);
     }
@@ -88,15 +55,13 @@ function imagesLoaded(){
 
 function buttonClicked(clickedId){
     var button = document.getElementById(clickedId);
-    var data = button.getAttribute('data-info');
+    var data = jQuery(button).data('info');
     var modal = document.getElementById('myModal');
     var span = document.getElementsByClassName("close")[0];
     var modal_cont = document.getElementById('myModalContent');
-    var tmp_arr = data.replace(' ', '');
-    tmp_arr = tmp_arr.split(',');
-    modal_cont.innerHTML = "<p>IP Address: " + tmp_arr[0] + "<br />" +
-        "MAC Address: " + tmp_arr[1] + "<br />" +
-        "Hostname: " + tmp_arr[10] + "</p>";
+    modal_cont.innerHTML = "<p>IP Address: " + data[0] + "<br />" +
+        "MAC Address: " + data[1] + "<br />" +
+        "Hostname: " + data[10] + "</p>";
     modal.style.display = "block";
     span.onclick = function () {
         modal.style.display = "none";
--- pcap_scanner_app/views.py
+++ pcap_scanner_app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 from pymongo import MongoClient
-
 # Create your views here.
 
 
@@ -14,8 +13,12 @@ class TopologyPageView(TemplateView):
     def get(self, request, **kwargs):
         client = MongoClient()
         db = client.ntm_db
-        devices = db.snmp_table.find()
-        return render(request, 'topology.html', {'device_list': devices}, content_type=list)
+        devices = db.snmp_table.find({}, {'_id': 0})
+        devices_arr = []
+        for document in devices:
+            device_list = list(document.values())
+            devices_arr.append(device_list)
+        return render(request, 'topology.html', {'device_list': devices_arr})
 
 
 class InventoryPageView(ListView):
@@ -23,4 +26,8 @@ class InventoryPageView(ListView):
         client = MongoClient()
         db = client.ntm_db
         details = db.snmp_table.find({}, {'_id': 0, 'hostname': 1, 'ip_addr': 1, 'mac_addr': 1})
-        return render(request, 'inventory.html', {'details_list': details})
+        details_arr = []
+        for document in details:
+            details_list = list(document.values())
+            details_arr.append(details_list)
+        return render(request, 'inventory.html', {'details_list': details_arr})
