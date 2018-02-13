21741ba195eeb21454a57294b64084f2d76e5f76
bwalsh <bwalsh362@gmail.com>
2018-02-12 18:50:18 -0700

    Implement Neighbors

--- scanner.py
+++ scanner.py
@@ -5,6 +5,7 @@ import datetime
 from struct import *
 from pcap_scanner_app import packet
 from pymongo import MongoClient
+import neighbors
 
 
 def main(argv):
@@ -33,6 +34,11 @@ def main(argv):
     while 1:
         (header, packet) = cap.next()
         parse_packet(packet)
+        get_neighbors()
+
+
+# def get_neighbors():
+#     neighbors.main()
 
 
 def connect_db(packet):
